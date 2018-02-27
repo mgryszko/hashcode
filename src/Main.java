1110f48974114c987a91c1cbdac164df2e6485dd
mgryszko <marcin.gryszko@lastminute.com>
2018-02-27 07:34:10 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import com.google.common.base.Strings;
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+    public static void main(String[] args) {
+        Strings.isNullOrEmpty("test");
+        StringUtils.abbreviate("adfadfasdf", 4);
+        System.out.println("Hello world!");
+    }
+}
